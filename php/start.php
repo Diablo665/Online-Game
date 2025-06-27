cc9002c1bcd9d9b303535348de486f3b6081c69e
Diablo665 <sarvirovr@gmail.com>
2025-06-27 08:23:02 +0300

    добавлены проверки передаваемого текста

--- php/start.php
+++ php/start.php
@@ -1,14 +1,19 @@
 <?php
 
-if (isset($_GET['type']) && isset($_GET['data'])) {
+$nick = isset($_GET['data']) ? htmlspecialchars($_GET['data'], ENT_QUOTES, 'UTF-8') : '';
+$type = isset($_GET['type']) ? htmlspecialchars($_GET['type'], ENT_QUOTES, 'UTF-8') : '';
+
+
+
+if (!empty($nick) && !empty($type)) {
     
-    $type = $_GET['type'];
     if ($type == 'enter'){ 
-        setcookie('Nick', $_GET['data'], array(
+        setcookie('Nick', $nick, array(
 	        'expires' => strtotime('+1 days'),
 	        'path' => '/',
 	        'secure' => true,  
 	        'httponly' => true,  
+            'samesite' => 'Strict'
         ));
 
         echo json_encode(array('success' => $_COOKIE['NickName']));
